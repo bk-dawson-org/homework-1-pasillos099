88ea91fad11986f3143bb1e4b2d22001aa3aae28
pasillos099 <ma.pasillos@dawsoncollege.qc.ca>
2024-09-20 06:41:19 +0000

    homeworksept20

---
+++ sept20homework1.py
@@ -0,0 +1,5 @@
+print("Enter the radius of sphere: ")
+radius = float(input()) 
+import math
+volume = (4/3) * math.pi * radius**3
+print(f"The volume of the sphere with radius {radius} is: {volume:.2f}")
